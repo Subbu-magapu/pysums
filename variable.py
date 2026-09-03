de5cfc796958c78bf4beea9f79a09339dab13b77
Subbu-magapu <subbumagapu03@gmail.com>
2026-09-03 23:17:21 +0530

    first commit

---
+++ variable.py
@@ -0,0 +1,11 @@
+#VARIABLES 
+#What is variables? 
+#Variables are used to store data in a program. They act as containers for values that can change during the execution of a program.
+#  In Python, you can create a variable by assigning a value to it using the equals sign (=).
+Name = "subbu"
+Age = 22
+city = "Rajahmundry"
+
+print("My name is", Name)
+print("I am", Age, "years old")
+print("I live in", city)
